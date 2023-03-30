61be3464c108383dadd6aa936067e5287d4df2d5
jothsnalatha <129383932+jothsnalatha@users.noreply.github.com>
2023-03-30 18:15:04 +0530

    Create site1.pp

---
+++ site1.pp
@@ -0,0 +1,17 @@
+if $::hostname == 'puppetclient4' {
+  file { '/tmp/jo1.txt':
+    ensure => present,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    content => "#Welcome",
+  }
+} else {
+  file { '/tmp/jo2.txt':
+    ensure => present,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    content => "# This is to welcome",
+  }
+}
